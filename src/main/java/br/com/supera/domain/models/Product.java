70b9b8213d76c83c45d0c5fb2f6fbe020b2f02c3
AdamAugustinsky <AdamAugustinsky@gmail.com>
2021-10-06 00:25:12 -0300

    add: Product model to domain

---
+++ src/main/java/br/com/supera/domain/models/Product.java
@@ -0,0 +1,15 @@
+package br.com.supera.domain.models;
+
+import java.math.BigDecimal;
+
+public class Product {
+    public long id;
+
+    public String name;
+
+    public BigDecimal price;
+
+    public short score;
+
+    public String image; 
+}
