8cecb3ba809517bd5c4f09d954ccff4e591a4e54
AdamAugustinsky <AdamAugustinsky@gmail.com>
2021-10-08 16:19:42 -0300

    add(CartRepository): increaseShippingPrice

--- src/main/java/br/com/supera/data/interfaces/CartRepository.java
+++ src/main/java/br/com/supera/data/interfaces/CartRepository.java
@@ -1,5 +1,7 @@
 package br.com.supera.data.interfaces;
 
+import java.math.BigDecimal;
+
 import br.com.supera.domain.dtos.AddProductDTO;
 import br.com.supera.domain.models.Cart;
 
@@ -7,4 +9,5 @@ public interface CartRepository {
     Cart findById(long cartId);
     Cart create();
     Cart addProduct(long cartId, AddProductDTO addProductDTO);
+    Cart increaseShippingPrice(long cartId, BigDecimal value);
 }
