1e70cae89089a513da59da439e61f0c74e4b3eb5
AdamAugustinsky <AdamAugustinsky@gmail.com>
2021-10-08 15:26:03 -0300

    test: add AddProductTest

---
+++ src/test/java/br/com/supera/AddProductTest.java
@@ -0,0 +1,58 @@
+package br.com.supera;
+
+import io.quarkus.test.junit.QuarkusTest;
+import io.restassured.http.ContentType;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.notNullValue;
+
+import javax.json.Json;
+import javax.json.JsonObject;
+
+@QuarkusTest
+public class AddProductTest {
+
+    @Test
+    public void createCart() {
+        //create new cart
+        given()
+        .when().post("/cart");
+
+        JsonObject testProduct = Json.createObjectBuilder()
+            .add("name", "test")
+            .add("price", 10.0)
+            .add("score", 10)
+            .add("image", "https://source.unsplash.com/user/c_v_r/100x100")
+            .build();
+        
+        given()
+        .pathParam("cartId", 1)
+        .body(testProduct.toString())
+        .contentType(ContentType.JSON)
+        .when().post("/cart/{cartId}")
+        .then()
+            .statusCode(201)
+            .body("products", notNullValue());
+    }
+
+    @Test
+    public void createCartNotFound() {
+        String testProduct = Json.createObjectBuilder()
+            .add("name", "test")
+            .add("price", 10.0)
+            .add("score", 10)
+            .add("image", "https://source.unsplash.com/user/c_v_r/100x100")
+            .build().toString();
+        
+        given()
+        .pathParam("cartId", 12)
+        .body(testProduct)
+        .contentType(ContentType.JSON)
+        .when().post("/cart/{cartId}")
+        .then()
+            .statusCode(404);
+    }
+
+}
