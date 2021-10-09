3e0ab0f2436039e0582092b7805ae0475ff43be6
AdamAugustinsky <AdamAugustinsky@gmail.com>
2021-10-09 18:24:59 -0300

    add(ListProducts): alphabeticalOrder parameter

--- src/main/java/br/com/supera/domain/useCases/ListProducts.java
+++ src/main/java/br/com/supera/domain/useCases/ListProducts.java
@@ -5,5 +5,5 @@ import java.util.List;
 import br.com.supera.domain.models.Product;
 
 public interface ListProducts {
-    List<Product> listAll(long cartId);
+    List<Product> listAll(long cartId, boolean alphabeticalOrder);
 }
