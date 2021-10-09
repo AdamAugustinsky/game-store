e70cd151a34c9a262413885a7e4604574daae234
AdamAugustinsky <AdamAugustinsky@gmail.com>
2021-10-09 17:56:50 -0300

    add: ListProducts to domain

---
+++ src/main/java/br/com/supera/domain/useCases/ListProducts.java
@@ -0,0 +1,9 @@
+package br.com.supera.domain.useCases;
+
+import java.util.List;
+
+import br.com.supera.domain.models.Product;
+
+public interface ListProducts {
+    List<Product> listAll(long cartId);
+}
