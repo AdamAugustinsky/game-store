0e745a3a94dd82e2282932db8ebcf73e03eeaa70
AdamAugustinsky <AdamAugustinsky@gmail.com>
2021-10-09 17:57:44 -0300

    add(CartRepository): ListProducts

--- src/main/java/br/com/supera/data/interfaces/CartRepository.java
+++ src/main/java/br/com/supera/data/interfaces/CartRepository.java
@@ -1,11 +1,15 @@
 package br.com.supera.data.interfaces;
 
+import java.util.List;
+
 import br.com.supera.domain.dtos.AddProductDTO;
 import br.com.supera.domain.models.Cart;
+import br.com.supera.domain.models.Product;
 
 public interface CartRepository {
     Cart findById(long cartId);
     Cart create();
     Cart addProduct(long cartId, AddProductDTO addProductDTO);
     Cart removeProduct(long cartId, long productId);
+    List<Product> listProducts(long cartId);
 }
