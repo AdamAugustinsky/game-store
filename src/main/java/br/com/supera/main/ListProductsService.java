6537b2d3c007126cc32667cdd8ab69160569835b
AdamAugustinsky <AdamAugustinsky@gmail.com>
2021-10-09 17:59:36 -0300

    add: ListProductsService

---
+++ src/main/java/br/com/supera/main/ListProductsService.java
@@ -0,0 +1,26 @@
+package br.com.supera.main;
+
+import java.util.List;
+
+import javax.enterprise.context.ApplicationScoped;
+
+import br.com.supera.data.useCases.ListProductsImpl;
+import br.com.supera.domain.models.Product;
+import br.com.supera.domain.useCases.ListProducts;
+import br.com.supera.infra.panache.repositories.PanacheCartRepository;
+
+@ApplicationScoped
+public class ListProductsService {
+    
+    private ListProducts listProducts;
+
+    public ListProductsService() {
+        PanacheCartRepository panacheCartRepository = new PanacheCartRepository();
+        this.listProducts = new ListProductsImpl(panacheCartRepository);
+    }
+
+    public List<Product> handle(long cartId) {
+        return listProducts.listAll(cartId);
+    }
+    
+}
