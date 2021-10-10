06c077a9dc9da87085f92546f5844e1ac9c28930
AdamAugustinsky <AdamAugustinsky@gmail.com>
2021-10-09 21:28:31 -0300

    add: CartCheckoutDTO

---
+++ src/main/java/br/com/supera/domain/dtos/CartCheckoutDTO.java
@@ -0,0 +1,11 @@
+package br.com.supera.domain.dtos;
+
+import java.math.BigDecimal;
+
+public class CartCheckoutDTO {
+    public BigDecimal shipping_price;
+
+    public BigDecimal subtotal;
+
+    public BigDecimal total;
+}
